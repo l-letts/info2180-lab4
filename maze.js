166900c780df44fd65c8be7719b441e9c37871e5
l-letts <larnelleletts11@outlook.com>
2016-10-14 12:46:52 -0700

    part_1

---
+++ maze.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.getElementById("boundary1").onmouseover = start;
+}
+ 
+function start()
+{
+    document.getElementById("boundary1").className="boundary youlose";
+}
+    
+        
+    
+
+
