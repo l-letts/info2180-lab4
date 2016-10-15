bc1c49cec674234965823c46791082eaf27757b2
l-letts <larnelleletts11@outlook.com>
2016-10-14 19:05:35 -0700

    I need alot more practice, best i could do

--- maze.js
+++ maze.js
@@ -1,13 +1,10 @@
-"use struct";
 var startValue = 0;
+var gameStatus = true;
 
-window.onload = function() 
-{
+window.onload = function () {
     document.getElementById("boundary1").onmouseover = start;
     
-    
-        var borders = document.querySelectorAll(".boundary"); 
-    
+    var borders = document.querySelectorAll(".boundary"); 
     for (var i = 0; i <borders.length; i++)
         
         {
@@ -42,6 +39,48 @@ document.getElementById("start").onmouseover = function()
         Flag = 1;
     }
 
+End.onmouseover = function()
+      {
+          
+          if(Flag === 1 && TouchWall === 0)
+          {
+              Status.innerHTML = "You Win!";
+          }
+      } 
 
+Start.addEventListener("click",function(){
+          
+          //Reset wall color
+          for (var i = 0; i < document.querySelectorAll('.boundary').length; i++)
+              {
+                  document.querySelectorAll('.boundary')[i].style.backgroundColor = "#eeeeee";   
+              }  
+          //Reset Flags and message
+          TouchWall = 0;
+          Status.innerHTML = "Move your mouse over the \"S\" to begin.";
+        
+        
+           MazeLeft = maze.offsetLeft;  
+           document.getElementById('maze').onmousemove = function()
+          {
+            Checker(event);  
+          }
+    
+      });
+
+
+    
+var startButton = document.getElementById("start");
+
+    startButton.addEventListener('click', function()
+    {
+        //alert('Game reset!');
+        document.getElementById("status").innerHTML = "Game reset!";
+        gameStatus = true;
+        for (var i = 0; i < boundaries.length; i++) 
+        {
+        boundaries[i].setAttribute('class','boundary');
+        }
+    });
    
   
